b99ed55e1a8e3400aeb008f1a42d01814106b85d
simjsik75 <simjsik75@naver.com>
2024-05-13 14:24:54 +0900

    main_header

---
+++ src/components/HeaderTop.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <div class="header_wrap">
+      <div class="header_logo">
+      </div>
+      <div class="header_menu_wrap">
+        <div class="header_product"></div>
+        <div class="header_viverse"></div>
+        <div class="header_business"></div>
+        <div class="header_support"></div>
+        <div class="header_community"></div>
+        <div class="header_accesorry"></div>
+      </div>
+      <div class="header_right_menu">
+        <div class="header_search"></div>
+        <div class="header_basket"></div>
+        <div class="header_profile"></div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({})
+export default class HeaderTop extends Vue {
+  msg!: string;
+}
+</script>
+
+<style scoped>
+</style>
+
