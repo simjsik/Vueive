80f7b9f2a7d19fd7a149103bb8372b4a9acb9188
simjsik75 <simjsik75@naver.com>
2024-05-13 14:50:47 +0900

    squash_header_to_main

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,29 @@
 <script>
 import HeaderTop from './components/HeaderTop.vue'
 
-export default {
-  name: 'App',
+@Options({
   components: {
     HeaderTop,
   }
-}
+})
+export default class App extends Vue {}
 </script>
 
 <style>
+@font-face {
+  font-family: "Pretendard-Medium";
+  src: url("https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Medium.woff")
+    format("woff");
+  font-weight: 500;
+  font-style: normal;
+}
+@font-face {
+  font-family: "Pretendard-Bold";
+  src: url("https://fastly.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Bold.woff")
+    format("woff");
+  font-weight: 700;
+  font-style: normal;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -21,5 +35,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+* {
+  font-family: "Pretendard-Medium";
+  font-size: 12px;
+  color: #090909;
+  box-sizing: border-box;
+  margin: 0 auto;
+  text-decoration: none;
+  font-style: normal;
+  list-style: none;
 }
 </style>
--- src/components/HeaderTop.vue
+++ src/components/HeaderTop.vue
@@ -4,12 +4,12 @@
       <div class="header_logo">
       </div>
       <div class="header_menu_wrap">
-        <div class="header_product"></div>
-        <div class="header_viverse"></div>
-        <div class="header_business"></div>
-        <div class="header_support"></div>
-        <div class="header_community"></div>
-        <div class="header_accesorry"></div>
+        <div class="header_product"><a href="#">제품</a></div>
+        <div class="header_viverse"><a href="#">VIVERSE</a></div>
+        <div class="header_business"><a href="#">비즈니스</a></div>
+        <div class="header_support"><a href="#">지원</a></div>
+        <div class="header_community"><a href="#">커뮤니티</a></div>
+        <div class="header_accesorry"><a href="#">액세서리</a></div>
       </div>
       <div class="header_right_menu">
         <div class="header_search"></div>
@@ -30,5 +30,47 @@ export default class HeaderTop extends Vue {
 </script>
 
 <style scoped>
-</style>
+header{
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 44px;
+  background: #F9F9FB;
+}
+.header_wrap{
+  width: 1000px;
+  height: 44px;
+  margin: 0 auto;
+  background: yellow;
+}
 
+.header_logo{
+  width: 44px;
+  height: 100%;
+  background: red;
+  float: left;
+}
+.header_menu_wrap{
+  float: left;
+  margin-left: 100px;
+}
+.header_menu_wrap>div{
+  float: left;
+  margin-right: 72px;
+  line-height: 44px;
+}
+.header_right_menu{
+  float: right;
+}
+.header_right_menu>div{
+  float: left;
+  width: 44px;
+  height: 44px;
+  background: red;
+  margin-right: 18px;
+}
+.header_right_menu>div:last-child{
+  margin-right: 0px;
+}
+</style>
